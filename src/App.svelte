bfecbb54d240ce810ba33b5cd9e8e03d568bdf50
Junjie Wang <microdou@gmail.com>
2021-02-22 11:03:16 -0500

    Test svelte-materialify ButtonGroup

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+import { Button, Icon, ButtonGroup, ButtonGroupItem } from "svelte-materialify";
+import { mdiHeart } from "@mdi/js";
 export let name: string;
+let value = 1;
 </script>
 
 <main>
@@ -8,6 +11,21 @@ export let name: string;
     Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
     how to build Svelte apps.
   </p>
+  <div class="text-center">
+    <Button class="pink-text">
+      <Icon path={mdiHeart} />
+      Hello
+    </Button>
+    <ButtonGroup activeClass="primary-color" bind:value>
+      <ButtonGroupItem>Left</ButtonGroupItem>
+      <ButtonGroupItem>Center</ButtonGroupItem>
+      <ButtonGroupItem>Right</ButtonGroupItem>
+      <ButtonGroupItem>Justify</ButtonGroupItem>
+    </ButtonGroup>
+    <br />
+    Value:
+    {value}
+  </div>
 </main>
 
 <style>
